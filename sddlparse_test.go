539b0974dc154db57fc865c18fe0d012c9df9f50
Hunter Ross <hrross86@gmail.com>
2023-12-01 07:45:23 -0600

    Removal of some test data

--- sddlparse_test.go
+++ sddlparse_test.go
@@ -9,50 +9,6 @@ import (
 	"github.com/huner2/go-sddlparse"
 )
 
-func TestB64SDDLLarge(t *testing.T) {
-	b64, err := os.ReadFile("./testdata/large_test_data.b64")
-	if err != nil {
-		t.Fatal(err)
-	}
-	sddl, err := sddlparse.SDDLFromBase64Encoded(b64)
-	if err != nil {
-		t.Fatal(err)
-	}
-	js, err := json.Marshal(sddl)
-	if err != nil {
-		t.Fatal(err)
-	}
-	compareJs, err := os.ReadFile("./testdata/large_test.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(js, compareJs) {
-		t.Fatal("json output is not equal")
-	}
-}
-
-func TestB64SDDLLargeStr(t *testing.T) {
-	b64, err := os.ReadFile("./testdata/large_test_data.b64")
-	if err != nil {
-		t.Fatal(err)
-	}
-	sddl, err := sddlparse.SDDLFromBase64Encoded(b64)
-	if err != nil {
-		t.Fatal(err)
-	}
-	str, err := sddl.MustString()
-	if err != nil {
-		t.Fatal(err)
-	}
-	compareStr, err := os.ReadFile("./testdata/large_test.sddl")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal([]byte(str), compareStr) {
-		t.Fatal("string output is not equal")
-	}
-}
-
 func TestB64SDDLSmall(t *testing.T) {
 	b64, err := os.ReadFile("./testdata/small_test_data.b64")
 	if err != nil {
@@ -128,25 +84,6 @@ func TestParseSDDLSmallStr(t *testing.T) {
 	}
 }
 
-func TestParseSDDLLargeStr(t *testing.T) {
-	sddl, err := os.ReadFile("./testdata/large_test.sddl")
-	if err != nil {
-		t.Fatal(err)
-	}
-	sddlStr := string(sddl)
-	sddlParsed, err := sddlparse.SDDLFromString(sddlStr, "0", "0")
-	if err != nil {
-		t.Fatal(err)
-	}
-	sddlParsedStr, err := sddlParsed.MustString()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if sddlParsedStr != sddlStr {
-		t.Fatal("sddl string is not equal")
-	}
-}
-
 func TestBinaryOutputSmall(t *testing.T) {
 	b64, err := os.ReadFile("./testdata/small_test_data.b64")
 	if err != nil {
@@ -169,36 +106,6 @@ func TestBinaryOutputSmall(t *testing.T) {
 		t.Fatal(err)
 	}
 	compareJs, err := os.ReadFile("./testdata/small_test.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(js, compareJs) {
-		t.Fatal("json output is not equal")
-	}
-}
-
-func TestBinaryOutputLarge(t *testing.T) {
-	b64, err := os.ReadFile("./testdata/large_test_data.b64")
-	if err != nil {
-		t.Fatal(err)
-	}
-	sddl, err := sddlparse.SDDLFromBase64Encoded(b64)
-	if err != nil {
-		t.Fatal(err)
-	}
-	bin, err := sddl.ToBase64Encoded()
-	if err != nil {
-		t.Fatal(err)
-	}
-	sddl, err = sddlparse.SDDLFromBase64Encoded(bin)
-	if err != nil {
-		t.Fatal(err)
-	}
-	js, err := json.Marshal(sddl)
-	if err != nil {
-		t.Fatal(err)
-	}
-	compareJs, err := os.ReadFile("./testdata/large_test.json")
 	if err != nil {
 		t.Fatal(err)
 	}
